05aabe9bc3cdd67d5b740154ae69ae814f742d37
RempelOiveira <rempel.oliveira@gmail.com>
2023-02-09 16:01:19 -0300

    [CA-6] - Monitoring

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ RESTfulPrometheusMetrics(app, restful_api, path="/metrics", metrics_decorator=me
 
 @metrics_auth.verify_password
 def verify_credentials(username, password):
-    return (username, password) == (os.getenv("PROMETHEUS_USER"), os.getenv("PROMETHEUS_PASSWORD"))
+    return (username, password) == (os.getenv("METRICS_USER"), os.getenv("METRICS_PASSWORD"))
 
 @app.errorhandler(404)
 def page_not_found(e):
